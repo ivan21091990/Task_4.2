93e93e08fc7b880bb6b0330d8fd6ba5cfa7054c3
AndreevIP <Andreev2102@inbox.ru>
2019-09-11 09:52:04 +0300

    Change Task

--- src/Sweets/Present.java
+++ src/Sweets/Present.java
@@ -4,17 +4,18 @@ public class Present {
 
     public static void main(String[] args) {
 
-        Fruits fruits = new Fruits("Апельсины",2.5,100,36.7);
+        Fruits fruits = new Fruits("Апельсины",2.5,100.5,36.7);
         Biscuit biscuit = new Biscuit("Юбилейное",1.5,70,"Песочное");
-        Chocolate chocolate = new Chocolate("Mars", 0.3,50, "Батончик");
-
+        Chocolate chocolate = new Chocolate("Mars", 0.3,50.78, "Батончик");
 
         Sweets [] newYearPresent = {fruits,biscuit,chocolate};
         System.out.println("Новогодний подарок содержит:");
         for (Sweets someSweets : newYearPresent){
-            System.out.println(someSweets.toString());
+            System.out.println(someSweets.toString() + ';');
         }
         System.out.println();
+        System.out.println("Общая стоимость подарка составляет: " + (fruits.getPrice() + biscuit.getPrice() + chocolate.getPrice()) + " рублей;");
+        System.out.println("Общий вес подарка составляет: " + (fruits.getWeight() + biscuit.getWeight() + chocolate.getWeight()) + " кг.");
 
     }
 }
